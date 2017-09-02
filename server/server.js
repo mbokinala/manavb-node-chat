028292c31b106c11248d6461a98f61852c6d2572
Manav Bokinala <manav.bokinala@gmail.com>
2017-09-01 20:22:04 -0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,10 +14,25 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit from Admin text Welcome to chat
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat',
+        createdAt: Date.now()
+    });
+
+    // socket.broadcast.emit from Admin text new user joined
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New user joined the chat.',
+        createdAt: Date.now()
+    });
+
     socket.on('createMessage', (newMessage) => {
         newMessage.createdAt = Date.now();
         console.log('createMessage:', JSON.stringify(newMessage, undefined, 2));
         io.emit('newMessage', newMessage);
+        // socket.broadcast.emit('newMessage', newMessage);
     });
 
     socket.on('disconnect', () => {
