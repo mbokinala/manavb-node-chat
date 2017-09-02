23b7c0ea6f10b9a22bff33f2a985f95cf1cf55ee
Manav Bokinala <manav.bokinala@gmail.com>
2017-09-01 21:16:04 -0700

    Updated js files

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,7 @@
 var socket = io();
 
 socket.on('connect', function () {
-    console.log('Connected to server.')
-    
+    console.log('Connected to server.') 
 });
 
 socket.on('disconnect', function () {
@@ -11,4 +10,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message:', JSON.stringify(message, undefined, 2));
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'Hi'
+}, function (data) {
+    console.log('Got it:', data);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[message]').val()
+    }, function () {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -16,15 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    // socket.emit from Admin text Welcome to chat
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    // socket.broadcast.emit from Admin text new user joined
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage:', JSON.stringify(message, undefined, 2));
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is ackowledgement for the message');
         // socket.broadcast.emit('newMessage', newMessage);
     });
 
